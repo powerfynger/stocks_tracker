efaf2a5b65f74db4a8226b33e10f854d17e691b3
powerfyng <puunika@mail.ru>
2024-08-18 15:23:09 +0300

    (feature) Изменен подход вывода обновленной информации

--- app/tg_bot.py
+++ app/tg_bot.py
@@ -2,6 +2,7 @@ import sys
 import os
 import asyncio
 from data_reciever import get_base_statistics
+from datetime import datetime
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -23,6 +24,17 @@ from config import Config
 
 bot = Bot(token=Config.TELEGRAM_BOT_TOKEN)
 
+user_states = {}
+
+last_message_id = None
+
+def create_menu():
+    keyboard = [
+        [InlineKeyboardButton("Получить статистику", callback_data='1')],
+    ]
+    return InlineKeyboardMarkup(keyboard)
+
+
 def get_recomendation_str(percantage):
     percantage = float(percantage)
     if percantage > 0.5:
@@ -31,6 +43,7 @@ def get_recomendation_str(percantage):
         return "Продавать"
     else:
         return "Неточно"
+
 def get_pretty_from_base_stats(data):
     summary = "**Топ-10 акций:**\n"
     summary += """
@@ -46,16 +59,36 @@ def get_pretty_from_base_stats(data):
         summary += f"    * Относительный объем: {stock_data['relative_volume_intraday|5']:.2f}\n"
         summary += f"    * RSI(15): {stock_data['RSI|15']:.0f}\n"
         summary += f"    * EMA10|5: {stock_data['EMA10|5']:.0f}\n"
+    
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    summary += f"\n\n*Дата обновления: {current_time}*"
     return summary
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user_tg = update.effective_user
+    global last_message_id
+    menu = create_menu()
 
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="Онлайн.",
-    )
+    message = await update.message.reply_text('Инициализация...', reply_markup=menu)
+    last_message_id = message.message_id  
+
+    asyncio.create_task(check_stat(context))
+
+# Обработчик нажатий на кнопки меню
+async def button_handler(update: Update, context):
+    query = update.callback_query
+    data = query.data
+
+    if data == '1':
+        data = get_base_statistics()
+        msg_text = get_pretty_from_base_stats(data)
+    else:
+        text = "Неизвестный выбор"
+
+    menu = create_menu()
+    await query.edit_message_text(text=msg_text, reply_markup=menu, parse_mode=ParseMode.MARKDOWN)
+
+    
 async def get_stat(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_tg = update.effective_user
     data = get_base_statistics()
@@ -68,23 +101,32 @@ async def get_stat(update: Update, context: ContextTypes.DEFAULT_TYPE):
         parse_mode=ParseMode.MARKDOWN
     )
 
-async def check_stat():
+async def check_stat(bot):
+    global last_message_id
+    chat_id = Config.TEST_CHAT_ID
+    menu = create_menu()
     while True:
         data = get_base_statistics()
         if data[0]['relative_volume_intraday|5'] > Config.MIN_RELATIVE_VOLUME_DAY:
             msg_text = get_pretty_from_base_stats(data)
-            await bot.send_message(chat_id=Config.TEST_CHAT_ID, text=msg_text, parse_mode=ParseMode.MARKDOWN)
+            if last_message_id:
+                try:
+                    await bot.edit_message_text(chat_id=chat_id, message_id=last_message_id, text=msg_text, parse_mode=ParseMode.MARKDOWN,reply_markup=menu)
+                except Exception as e:
+                    print(f"Ошибка при обновлении сообщения: {e}")
+            else:
+                # Если last_message_id нет, отправляем новое сообщение и сохраняем его ID
+                new_message = await bot.send_message(chat_id=chat_id, text=msg_text, parse_mode=ParseMode.MARKDOWN, reply_markup=menu)
+                last_message_id = new_message.message_id
         await asyncio.sleep(60)
-
 def main():
     application = Application.builder().token(Config.TELEGRAM_BOT_TOKEN).build()
+    
     application.add_handler(CommandHandler('start', start))
-
-    application.add_handler(CommandHandler('stat', get_stat))
-
+    application.add_handler(CallbackQueryHandler(button_handler))
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    loop.create_task(check_stat())
+    loop.create_task(check_stat(application.bot))
     application.run_polling()
 
 if __name__ == "__main__":
